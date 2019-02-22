6bc36587542a8b66363b96b77aa3f156faf5b39d
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-02-22 14:30:17 -0600

    Entities CRUD added

--- entities/urls.py
+++ entities/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from entities.views import HomeView, NewEntityView, EditEntityView
+from entities.views import HomeView, NewEntityView, EditEntityView, EntityView, DeleteEntityView
 
 app_name = 'entities'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('new/', NewEntityView.as_view(), name='new'),
-    path('<int:id>/edit/', EditEntityView.as_view(), name='edit')
+    path('<int:id>/', EntityView.as_view(), name='entity'),
+    path('<int:id>/edit/', EditEntityView.as_view(), name='edit'),
+    path('<int:id>/delete/', DeleteEntityView.as_view(), name='delete')
 ]
--- entities/views.py
+++ entities/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, UpdateView, CreateView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, UpdateView, CreateView, DeleteView
 from entities.models import Entity
+from django.urls import reverse_lazy
 
 
 class HomeView(TemplateView):
@@ -9,6 +10,15 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         entities = Entity.objects.all()
         return dict(entities=entities)
+
+
+class EntityView(TemplateView):
+    template_name = 'entities/entity.html'
+
+    def get_context_data(self, **kwargs):
+        entity = get_object_or_404(Entity, pk=kwargs['id'])
+
+        return dict(entity=entity)
 
 
 class NewEntityView(CreateView):
@@ -31,3 +41,10 @@ class EditEntityView(UpdateView):
     def form_valid(self, form):
         entity = form.save()
         return redirect('entities:edit', entity.pk)
+
+
+class DeleteEntityView(DeleteView):
+    model = Entity
+    template_name = 'entities/delete.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('entities:index')
