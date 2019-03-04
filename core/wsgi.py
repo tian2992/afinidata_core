52b7ec3d1829523215eddecdcece2cedca1b4de9
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-03-04 16:12:26 -0600

    Added dotenv file reference to uwsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
+
+env_path = '../.env'
+load_dotenv(dotenv_path=env_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
