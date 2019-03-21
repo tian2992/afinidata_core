819715b9b2057beb7302eb2a099f4751d528600d
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-03-20 19:27:05 -0600

    Many to many instances with attributes trougth AttributesValue

--- instances/models.py
+++ instances/models.py
@@ -3,21 +3,20 @@ from entities.models import Entity
 from bots.models import Bot
 from areas.models import Area
 from milestones.models import Milestone
+from attributes.models import Attribute
 
 
 class Instance(models.Model):
     entity = models.ForeignKey(Entity, on_delete=models.CASCADE)
     bot = models.ForeignKey(Bot, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    attributes = models.ManyToManyField(Attribute, through='AttributeValue')
     bot_user_id = models.IntegerField(default=1, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        app_label = 'instances'
 
 
 class Score(models.Model):
@@ -30,9 +29,6 @@ class Score(models.Model):
     def __str__(self):
         return self.instance.name + '__' + self.area.name + '__' + str(round(self.value, 2))
 
-    class Meta:
-        app_label = 'instances'
-
 
 class ScoreTracking(models.Model):
     instance = models.ForeignKey(Instance, on_delete=models.CASCADE)
@@ -44,9 +40,6 @@ class ScoreTracking(models.Model):
     def __str__(self):
         return self.instance.name + '__' + self.area.name + '__' + str(round(self.value, 2))
 
-    class Meta:
-        app_label = 'instances'
-
 
 class Response(models.Model):
     instance = models.ForeignKey(Instance, on_delete=models.CASCADE)
@@ -55,8 +48,16 @@ class Response(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        app_label = 'instances'
-
     def __str__(self):
         return str(self.pk) + '__' + self.instance.name + '__response'
+
+
+class AttributeValue(models.Model):
+    instance = models.ForeignKey(Instance, on_delete=models.CASCADE)
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    value = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s__%s__%s__%s" % (self.pk, self.instance.name, self.attribute.name, self.value)
