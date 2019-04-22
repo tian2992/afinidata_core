89aa006c6766cddbaba883f0b71371cf052de901
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-04-22 17:01:01 -0600

    convert months group in set

--- messenger_users/views.py
+++ messenger_users/views.py
@@ -36,7 +36,10 @@ class ByGroupView(LoginRequiredMixin, ListView):
         context = super(ByGroupView, self).get_context_data(**kwargs)
         context['total'] = User.objects.filter(userdata__data_key='AB_group',
                                                userdata__data_value=self.kwargs['group']).count()
-        context['months_groups'] = UserData.objects.filter(data_key='months_group').order_by('data_value')
+        months_group = set(item.data_value for item in UserData.objects.filter(data_key='months_group')
+                           .order_by('data_value'))
+        context['months_groups'] = months_group
+        print(context)
         return context
 
 
