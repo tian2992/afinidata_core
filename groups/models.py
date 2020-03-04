abd1517fcc47aeb6cd857c021ee082626bad014e
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-03-04 12:46:02 -0600

    gitflow-feature-stash: messenger_users_groups
    Added messenger users list in group. Added manual assignations.

--- groups/models.py
+++ groups/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from messenger_users.models import User as MessengerUser
 from django.db import models
 
 
@@ -18,3 +19,21 @@ class RoleGroupUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Code(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    code = models.CharField(max_length=50, unique=True)
+    available = models.BooleanField(default=True)
+    exchanges = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class AssignationMessengerUser(models.Model):
+    messenger_user_id = models.IntegerField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    code_id = models.ForeignKey(Code, null=True, on_delete=models.SET_NULL)
+
+    def get_messenger_user(self):
+        return MessengerUser.objects.get(id=self.messenger_user_id)
--- groups/urls.py
+++ groups/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from groups import views
 from groups import roles_views
+from groups import messenger_users_views
 
 app_name = 'groups'
 
 urlpatterns = [
     path('', views.GroupListView.as_view(), name='groups'),
-    path('<int:group_id>/add_user/', roles_views.CreateRoleView.as_view(), name='add_user_group'),
     path('<int:group_id>/', views.GroupView.as_view(), name='group'),
+    path('<int:group_id>/add_user/', roles_views.CreateRoleView.as_view(), name='add_user_group'),
+    path('<int:group_id>/add_messenger_user/', messenger_users_views.AddMessengerUserView.as_view(),
+         name='add__messenger_user_group'),
     path('add/', views.CreateGroupView.as_view(), name='add')
 ]
