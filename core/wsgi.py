3092ce1e24fc1469441824134b501f37dc3c4898
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-03-10 16:08:29 -0600

    changed wsgi and settings

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,10 +8,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
-load_dotenv(dotenv_path='~/afinidata_core/.env')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.production')
 
