4015d7b1bcfb7bbb7d1aa3f3aef89dbc3c3e5b7d
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-03-13 17:43:53 -0600

    Milestones working

--- milestones/forms.py
+++ milestones/forms.py
@@ -1,23 +1,2 @@
 from django import forms
-from milestones.models import Milestone
-from instances.models import Instance
 
-
-class MilestoneFormModel(forms.ModelForm):
-
-    class Meta:
-        model = Milestone
-        fields = ('name', 'code', 'area',  'value', 'secondary_value', 'description')
-
-    def __init__(self, *args, **kwargs):
-        super(MilestoneFormModel, self).__init__(*args, **kwargs)
-        self.fields['description'].required = False
-
-
-class ResponseMilestoneForm(forms.Form):
-    RESPONSE_CHOICES = (
-        ('true', 'yes'),
-        ('false', 'no')
-    )
-    instance = forms.ModelChoiceField(queryset=Instance.objects.all())
-    response = forms.ChoiceField(choices=RESPONSE_CHOICES)
--- milestones/views.py
+++ milestones/views.py
@@ -1,13 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views import View
-from django.views.generic import TemplateView, UpdateView, ListView, CreateView
-from milestones.forms import MilestoneFormModel, ResponseMilestoneForm
-from milestones.models import Milestone, Step
-from instances.models import Instance, Response, Score
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
+from django.views.generic import DetailView, UpdateView, ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from milestones.models import Milestone
 from django.urls import reverse_lazy
 from django.contrib import messages
 
@@ -19,14 +12,10 @@ class HomeView(LoginRequiredMixin, ListView):
     paginate_by = 50
 
 
-class MilestoneView(LoginRequiredMixin, TemplateView):
+class MilestoneView(LoginRequiredMixin, DetailView):
     login_url = reverse_lazy('pages:login')
-    template_name = 'milestones/milestone.html'
-
-    def get_context_data(self, **kwargs):
-        milestone = get_object_or_404(Milestone, pk=kwargs['id'])
-
-        return dict(milestone=milestone)
+    model = Milestone
+    pk_url_kwarg = 'id'
 
 
 class EditMilestoneView(LoginRequiredMixin, UpdateView):
@@ -59,51 +48,3 @@ class NewMilestoneView(LoginRequiredMixin, CreateView):
         c = super(NewMilestoneView, self).get_context_data()
         c['action'] = 'Create'
         return c
-
-
-@csrf_exempt
-def response_instance_to_milestone(request, id):
-
-    if request.method == 'GET':
-        return JsonResponse(dict(status='error', error='invalid params'))
-
-    print(request.POST)
-
-    milestone = get_object_or_404(Milestone, pk=id)
-
-    form = ResponseMilestoneForm(request.POST)
-
-    if form.is_valid():
-        print('valid')
-        instance = Instance.objects.get(id=request.POST['instance'])
-        response = request.POST['response']
-
-        if response == 'true':
-            old_score = None
-            new_value = milestone.value
-            try:
-                old_score = Score.objects.get(instance=instance, area=milestone.area)
-                if old_score.value > milestone.value:
-                    new_value = old_score.value
-            except Exception as e:
-                print(e)
-                pass
-            score = Score.objects.update_or_create(instance=instance, area=milestone.area, defaults=dict(
-                instance=instance,
-                area=milestone.area,
-                value=new_value
-            ))
-            print(score)
-
-        new_response = Response.objects.create(milestone=milestone, instance=instance, response=response)
-        print(new_response)
-        return JsonResponse(dict(
-            status='finished',
-            data=dict(
-                area=milestone.area.pk,
-                instance=instance.pk
-            )
-        ))
-
-    else:
-        return JsonResponse(dict(status='error', error='invalid params'))
